2e0487bfb9c57597cd5c1680350cd257d95da7cb
GorjanGOA <kalauzovic.gorjan@gmail.com>
2015-04-08 14:30:38 +0200

    Added list view

--- app/src/main/java/com/example/edibimamovic/bitgo/Setting.java
+++ app/src/main/java/com/example/edibimamovic/bitgo/Setting.java
@@ -1,20 +1,34 @@
 package com.example.edibimamovic.bitgo;
 
-import android.content.Intent;
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Setting extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent it= new Intent(this,Setting.class);
-        startActivity(it);
+        setContentView(R.layout.activity_setting);
+
+        ArrayList<String> info = new ArrayList<String>();
+        info.add("Username");
+        info.add("Location");
+        info.add("City");
+        info.add("Home");
+        info.add("Class");
+        info.add("Gym");
+
+        ArrayAdapter<String> adapterList = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2,info);
+        ListView settingsList=(ListView) findViewById(R.id.settings_list);
+        settingsList.setAdapter(adapterList);
 
     }
 
@@ -22,7 +36,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_setting, menu);
         return true;
     }
 
